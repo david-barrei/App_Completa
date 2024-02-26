485f1510b27c7e42fe58b7856e21bb0bbd7cd8b3
david-barrei <davbarreiro4@gmail.com>
2024-02-25 23:41:13 -0500

    coment

--- proyecto1/persona/urls.py
+++ proyecto1/persona/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('',views.Inicio.as_view(), name='inicio'),
     path('lista/',views.Lista.as_view(), name='lista'),
     path('crear/',views.Crear.as_view(), name='crear'),
-    path('editar/<pk>',views.Editar.as_view(), name='editar'),
-    path('eliminar/<pk>',views.Eliminar.as_view(), name='eliminar'),
+    path('editar/<pk>',views.Editar.as_view(), name='editar'),#Id por parametro
+    path('eliminar/<pk>',views.Eliminar.as_view(), name='eliminar'),#Pasar por parametro el id 
     
 ]#urls 
